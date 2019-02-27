c994844b43aba3c7ae9d3385b4460006b49e204d
Piotrek Witek <piotrek.witek@gmail.com>
2019-02-27 22:37:30 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,13 +5,15 @@ import Todos from '../features/todos/components/main';
 export default () => (
   <React.Fragment>
     <h1>Welcome to "typesafe-actions" todo-app!</h1>
-    <h2>⚠️ ⚠️ ⚠️ WARNING!!! THIS REPO IS NOW OUTDATED!!! ⚠️ ⚠️ ⚠️</h2>
+    <h2>⚠️⚠️⚠️ WARNING ️⚠️⚠️️⚠️️</h2>
+    <h3>THIS SANDBOX IS NOW OUTDATED</h3>
 
-    <p>There is a brand new <b>UP-TO-DATE</b> version including data fetching examples
-    located in the `typesafe-actions` repo: <a href="https://github.com/piotrwitek/typesafe-actions/tree/master/codesandbox">Link</a>
+    <p>There is a  new <b>UP-TO-DATE</b> version (including data fetching examples)
+    located in the <a href="https://github.com/piotrwitek/typesafe-actions">typesafe-actions</a> repo: <a href="https://github.com/piotrwitek/typesafe-actions/tree/master/codesandbox">LINK</a>
     </p>
 
-    <p>And here is a new CodeSandbox: <a href="https://codesandbox.io/s/github/piotrwitek/typesafe-actions/tree/master/codesandbox">Link</a>
+    <p>And here is a new CodeSandbox: <a href="https://codesandbox.io/s/github/piotrwitek/typesafe-actions/tree/master/codesandbox">LINK</a>
+    </p>
     <Todos />
   </React.Fragment>
 );
